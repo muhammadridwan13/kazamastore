c6d3a9a2935863e6c073d7caebe33dfe4432cad3
Muhammad Riduan <inidipuy@gmail.com>
2023-05-21 13:49:49 +0800

    first

---
+++ script.js
@@ -0,0 +1,63 @@
+// copy menu for mobile 
+
+const copyMenu = () => {
+    // copy dptcat to dpt ....
+    var dptCategory = document.querySelector('.dpt-cat')
+    var dptPlace = document.querySelector('.departments')
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    // copy inside nav to nav
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    // copy ...
+    var topNav = document.querySelector('.header-top .wrapper');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
+}
+
+copyMenu();
+
+// show mobile menu 
+const menuButton = document.querySelector('.trigger'),
+        closeButton = document.querySelector('.t-close'),
+        addclass = document.querySelector('.site');
+menuButton.addEventListener('click', function() {
+    addclass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click', function() {
+    addclass.classList.remove('showmenu')
+})
+
+// show submenu on mobile
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+    if (this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand');
+}
+
+// slider
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+});
+
+// show search
+const searchButton = document.querySelector('.t-search'),
+    tClose = document.querySelector('.search-close'),
+    showClass = document.querySelector('.site');
+searchButton.addEventListener('click', function() {
+    showClass.classList.toggle('showsearch')
+})
+tClose.addEventListener('click', function() {
+    showClass.classList.remove('showsearch')
+})
